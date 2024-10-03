11c8a3073755d7891f2584ed459e2462176ce8c4
APISITMMIE <mmie.naja@hotmaile.com>
2024-10-04 03:46:44 +0700

    first commit

---
+++ addForm.php
@@ -0,0 +1,50 @@
+<?php 
+    $title = "หน้าบันทึกข้อมูลพนักงงาน";
+    require_once "layout/header.php";
+    require_once "db/connect.php";
+    require_once "layout/check_admin.php";
+    $result = $controller->getDepartments();
+
+    if(isset($_POST["submit"])){
+        $fname = $_POST["fname"];
+        $lname = $_POST["lname"];
+        $salary = $_POST["salary"];
+        $department_id = $_POST["department_id"];
+        $status = $controller->insert($fname,$lname,$salary,$department_id);
+        if($status){
+            $message = "บันทึกข้อมูลเรียบร้อยแล้ว";
+            require_once "layout/success_message.php";
+        }else {
+            require_once "layout/error_message.php";
+        }
+    }
+?>
+<body>
+    <div class="container">
+        <h1 class="text-center my-3"><?php echo "แบบฟอร์มบันทึกข้อมูล"; ?></h1>
+        <form method="POST" action="addForm.php">
+            <div class="form-group">
+                <label for="fname">ชื่อ</label>
+                <input type="text" name="fname" class="form-control">
+            </div>
+            <div class="form-group my-3">
+                <label for="lname">นามสกุล</label>
+                <input type="text" name="lname" class="form-control">
+            </div>
+            <div class="form-group my-3">
+                <label for="salary">เงินเดือน</label>
+                <input type="text" name="salary" class="form-control">
+            </div>
+            <div class="form-group my-3">
+                <label for="department">แผนก</label>
+                <select name="department_id" class="form-control">
+                    <?php while($row = $result->fetch(PDO::FETCH_ASSOC)) {?>
+                        <option value="<?php echo $row["department_id"] ?>"><?php echo $row["department_name"]; ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+            <input type="submit" name="submit" value="บันทึก" class="btn btn-primary my-3">
+        </form>
+    </div>
+</body>
+</html>
---
+++ loginForm.php
@@ -0,0 +1,37 @@
+<?php 
+    $title = "แบบฟอร์มลงชื่อเข้าใช้";
+    require_once "layout/header.php";
+    require_once "db/connect.php";
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $new_password = md5($password.$username);
+        $result = $user->getUser($username,$new_password);
+        
+        if(!$result) {
+            echo '<div class="alert alert-danger">ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง</div>';
+        }else {
+            $_SESSION["username"] = $username;
+            $_SESSION["userid"] = $result["id"];
+            header("Location:index.php");
+        }
+    }
+?>
+<body>
+    <div class="container">
+        <h1 class="text-center my-3"><?php echo "แบบฟอร์มลงชื่อเข้าใช้"; ?></h1>
+        <form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']) ?>">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" value="<?php if($_SERVER["REQUEST_METHOD"]=="POST") echo $_POST["username"]; ?>" 
+                class="form-control">
+            </div>
+            <div class="form-group my-3">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <input type="submit" name="submit" value="เข้าสู่ระบบ" class="btn btn-primary my-3">
+        </form>
+    </div>
+</body>
+</html>
